e97565a95c229a9e2702b6239a146a60cd942238
Amadeus Wieczorek <git@amadeusw.com>
2014-12-28 13:47:50 -0800

    Adds function that loads any page into element

--- scripts/retriever.js
+++ scripts/retriever.js
@@ -1,20 +1,23 @@
 $( document ).ready(function() {
   console.log("ready");
+  loadPageIntoElement("Copenhagen", "#page1");
+});
+
+function loadPageIntoElement(page, element) {
 	$.ajax({
-	    url : "http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?&continue=&page=Copenhagen",
+	    url : "http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?&continue=&page=" + page,
 	    data: {
 	    	format: 'json'
 	    },
 	    dataType: 'jsonp',
 	    success : function(result){
 	        console.log("success");
-	        console.log(result);
 	    }
 	})
 	.done(function(data) {
-		console.log("done");
+		console.log("injecting into " + element);
 		console.log(data);
-		$( "#section_0" ).append( data.parse.displaytitle );
-		$( "#content" ).append( data.parse.text['*'] );
+		$( element + " > .pre-content > h1" ).append( data.parse.displaytitle );
+		$( element + " > .content" ).append( data.parse.text['*'] );
 	});  
-});
+}
