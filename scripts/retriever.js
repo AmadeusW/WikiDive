0000d0237bc449ef34b3001037f18ca9925bff8e
Amadeus Wieczorek <git@amadeusw.com>
2014-12-28 16:16:37 -0800

    Dynamically adds reading panes

--- scripts/retriever.js
+++ scripts/retriever.js
@@ -1,8 +1,29 @@
+var pageIndex = 0;
+
 $( document ).ready(function() {
   console.log("ready");
-  loadPageIntoElement("Copenhagen", "#page1");
-  loadPageIntoElement("Amadeus", "#page2");
+  loadPageIntoElement("Copenhagen", addPage());
+  loadPageIntoElement("Amadeus", addPage());
 });
+
+function addPage() {
+	var pageId = "page" + pageIndex++;
+	console.log("Creating element " + pageId);
+
+	var tempContainer = document.createElement("div");
+	tempContainer.innerHtml = 
+'			<div class="wikibrowser-page-host">' +
+'				<div class="wikibrowser-page ps-container ps-active-y" id="' + pageId + '">'+
+'					<div class="pre-content">'+
+'						<h1 id="section_0"></h1>'+
+'					</div>'+
+'					<div id="content" class="content"></div>'+
+'				</div>'+
+'			</div>';
+
+	$(".wikibrowser-host").append(tempContainer.innerHtml);
+	return pageId;
+}
 
 function loadPageIntoElement(page, element) {
 	$.ajax({
@@ -18,9 +39,9 @@ function loadPageIntoElement(page, element) {
 	.done(function(data) {
 		console.log("injecting into " + element);
 		console.log(data);
-		$( element + " > .pre-content > h1" ).append( data.parse.displaytitle );
-		$( element + " > .content" ).append( data.parse.text['*'] );
-		$( element ).perfectScrollbar({
+		$( "#" + element + " > .pre-content > h1" ).append( data.parse.displaytitle );
+		$( "#" + element + " > .content" ).append( data.parse.text['*'] );
+		$( "#" + element ).perfectScrollbar({
 			wheelSpeed: 3
 		});
 	});  
