14c4813423ba1efaff201add019361f9ec58e667
Amadeus Wieczorek <git@amadeusw.com>
2015-01-25 13:18:03 -0800

    Creates an ASP.MVC solution

---
+++ src/WikiDive/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WikiDive.Startup))]
+namespace WikiDive
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
